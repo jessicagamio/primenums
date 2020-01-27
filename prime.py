33a428a51e521e3afd2a3f65b713f4250bf9198b
jessica.gamio@gmail.com <jessica.gamio@gmail.com>
2020-01-27 13:06:31 -0800

    developing prime number psuedocode

--- prime.py
+++ prime.py
@@ -1,5 +1,27 @@
+
+### find prime number function
+
+## prime number when it is a multiple of 1 and itself
+## set a counter
+## from a range of 1 to num_from_list
+    #divide num from of num_from_list
+    # if divisible add to counter
+    # if counter greater than 1
+        # return False
+    # return True
+
+## finding the 2nd digit of birthdate
+
+
+### main function
 # parameters list and date
 # given a list of numbers return only a list prime nums that match the 2nd digit of birth date
 
+#create prime list
+
 #isolate the second digit
-#
+
+# iterate through the give list
+    #if the num in list is not a prime num and equal to second digit
+        #append to list
+
